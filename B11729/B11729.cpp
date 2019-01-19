11fb090eb2775f3023818cccc58369fcf3662ad6
tongnamuu <tongnamuu>
2019-01-20 02:58:50 +0900

    BOJ 11729 하노이 탑 이동 순서

---
+++ B11729/B11729.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+void go(int start, int end, int n) {
+	if (n == 1) {
+		cout << start << ' ' << end << '\n';
+		return;
+	}
+	int empty = 6 - start - end;
+	go(start, empty, n - 1);
+	cout << start << ' ' << end << '\n';
+	go(empty, end, n - 1);
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+	int s1 = 1;
+	int s2 = 1;
+	int t = n;
+	while (--t) {
+		s2 = 2 * s1 + 1;
+		s1 = s2;
+	}
+	cout << s2 << '\n';
+	go(1, 3, n);
+}
