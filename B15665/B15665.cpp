5b80c3878bdb29d8214656a1bf56f0c83a7c5bed
tongnamuu <tongnamuu>
2019-02-09 22:47:09 +0900

    BOJ 15665 N과 M(11)

---
+++ B15665/B15665.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int n, m;
+int a[10];
+vector<int>temp;
+vector<vector<int>>ans;
+void go(int cnt) {
+	if (cnt == m) {
+		ans.push_back(temp);
+		return;
+	}
+	for (int i = 0; i < n; ++i) {
+		temp.push_back(a[i]);
+		go(cnt + 1);
+		temp.pop_back();
+	}
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0), cout.tie(0);
+	cin >> n >> m;
+	for (int i = 0; i < n; ++i) cin >> a[i];
+	sort(a, a + n);
+	for (int i = 0; i < n; ++i) {
+		temp.push_back(a[i]);
+		go(1);
+		temp.pop_back();
+	}
+	sort(ans.begin(), ans.end());
+	ans.erase(unique(ans.begin(), ans.end()), ans.end());
+	for (auto v : ans) {
+		for (int i = 0; i < m; ++i) cout << v[i] << ' ';
+		cout << '\n';
+	}
+}
