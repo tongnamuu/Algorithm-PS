0b6b2416a22180e323d05b8b5f4d27533cac6670
tongnamuu <33937684+thban33@users.noreply.github.com>
2019-03-07 14:20:45 +0900

    SWEA 7206. 숫자 게임

--- S7206/S7206.cpp
+++ S7206/S7206.cpp
@@ -11,11 +11,12 @@ int getlength(int num) {
 	return len;
 }
 int ten[] = { 10,100,1000,10000,100000,1000000 };
-void go(int num, int turn) {
+int memo[1000000];
+int go(int num) {
 	if (num < 10) {
-		if (turn > ans) ans = turn;
-		return;
+		return 0;
 	}
+	int maxcnt = 0;
 	int len = getlength(num) - 1;
 	for (int i = 1; i < (1 << len); ++i) {
 		int temp = 1;
@@ -26,16 +27,18 @@ void go(int num, int turn) {
 				now %= ten[j];
 			}
 		}
-		go(temp*now, turn + 1);
+		int next = temp * now;
+		if (next > 10 && memo[next] == 0) memo[next]=go(next);
+		if (maxcnt < memo[next]) maxcnt = memo[next];	
 	}
+	return maxcnt + 1;
 }
 int main() {
 	ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
 	int T; cin >> T;
 	for (int test_case = 1; test_case <= T; ++test_case) {
 		int n; cin >> n;
-		ans = 0;
-		go(n, 0);
+		ans = go(n);
 		cout << '#' << test_case << ' ' << ans << '\n';
 	}
 }
