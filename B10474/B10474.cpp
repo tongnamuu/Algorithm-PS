3e398436bf1369e201ea761460daba0ea491f5c9
tongnamuu <33937684+thban33@users.noreply.github.com>
2019-03-18 09:31:37 +0900

    BOJ 10474 분수좋아해?

---
+++ B10474/B10474.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+	while (1) {
+		int a, b; cin >> a >> b;
+		if (a == 0 && b == 0) break;
+		cout << a / b << ' ' << a % b << " / " << b << '\n';
+	}
+}
