541cd1c1e9d40c6fbd7446053af7a66352916ba1
tongnamuu <33937684+thban33@users.noreply.github.com>
2019-03-23 13:33:31 +0900

    BOJ 9655 돌 게임

--- B9655/B9655.cpp
+++ B9655/B9655.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
+	ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
 	int n;
 	cin >> n;
 	if (n & 1) cout << "SK" << '\n';
