9b6c7c44318d9fb546f833972ebcb965db738afb
tongnamuu <33937684+thban33@users.noreply.github.com>
2019-03-23 14:00:45 +0900

    SWEA 1244. [S/W 문제해결 응용] 2일차 - 최대 상금

--- S1244/S1244.cpp
+++ S1244/S1244.cpp
@@ -37,8 +37,7 @@ void go(int cnt) {
 	}
 }
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0), cout.tie(0);
+	ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
 	int T; cin >> T;
 	for (int test_case = 1; test_case <= T; ++test_case) {
 		char s[7];
