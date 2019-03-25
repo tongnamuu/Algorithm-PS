860657a09ca711209d60f3a4e734e9ba4a7b06c8
tongnamuu <33937684+thban33@users.noreply.github.com>
2019-03-25 09:33:45 +0900

    BOJ 10101 삼각형 외우기

---
+++ B10101/B10101.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a, b, c; cin >> a >> b >> c;
+	if (a + b + c == 180) {
+		if (a == b && a == c) cout << "Equilateral" << '\n';
+		else if (a != b && b != c && c != a) cout << "Scalene" << '\n';
+		else cout << "Isosceles" << '\n';
+	}
+	else cout << "Error" << '\n';
+}
