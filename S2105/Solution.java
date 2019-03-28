73c3100dde70bdb6c4f485a981bb5a7851bbc741
tongnamuu <33937684+tongnamuu@users.noreply.github.com>
2019-03-28 09:56:15 +0900

    SWEA 2105. [모의 SW 역량테스트] 디저트 카페

--- S2105/Solution.java
+++ S2105/Solution.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Scanner;
 
 public class Solution {
@@ -10,41 +8,49 @@ public class Solution {
 	static int dx[] = { 1, -1, -1, 1 };
 	static int dy[] = { 1, 1, -1, -1 };
 	static int sx, sy;
+	
 	public static void go(int x, int y, int dir, int val) {
-		if(dir<4) {
-		int nx = x + dx[dir];
-		int ny = y + dy[dir];
-		if (nx >= 0 && nx < n&&ny >= 0 && ny < n) {
-			if (!check[a[nx][ny]]) {
-				check[a[nx][ny]] = true;
-				go(nx, ny, dir + 1, val + 1);
-				go(nx, ny, dir, val + 1);
-				check[a[nx][ny]] = false;
+		if (dir < 4) {
+			int nx = x + dx[dir];
+			int ny = y + dy[dir];
+			if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
+				if (!check[a[nx][ny]]) {
+					check[a[nx][ny]] = true;
+					go(nx, ny, dir + 1, val + 1);
+					go(nx, ny, dir, val + 1);
+					check[a[nx][ny]] = false;
+				} else if (nx == sx && ny == sy) {
+					if (val > ans)
+						ans = val;
+					return;
+				} else
+					return;
 			}
-			else if (nx == sx && ny == sy) {
-				if (val > ans) ans = val;
-				return;
-			}
-			else return;
-		}
-		}
-		else return;
+		} else
+			return;
 	}
+
 	public static void main(String[] args) {
-		Scanner sc =new Scanner(System.in);
-		int T=sc.nextInt();
-		for(int test_case=1;test_case<=T;++test_case) {
-			n=sc.nextInt();
-			ans=-1;
-			a=new int[n][n];
-			check=new boolean[101];
-			for(int i=0;i<n;++i)for(int j=0;j<n;++j) a[i][j]=sc.nextInt();
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int test_case = 1; test_case <= T; ++test_case) {
+			n = sc.nextInt();
+			ans = -1;
+			a = new int[n][n];
+			check = new boolean[101];
+			for (int i = 0; i < n; ++i)
+				for (int j = 0; j < n; ++j)
+					a[i][j] = sc.nextInt();
 			for (int i = 0; i < n; ++i) {
 				for (int j = 0; j < n; ++j) {
-					if (i == 0 && j == n - 1) continue;
-					else if (i == 0 && j == 0) continue;
-					else if (i == n - 1 && j == 0) continue;
-					else if (i == n - 1 && j == n - 1) continue;
+					if (i == 0 && j == n - 1)
+						continue;
+					else if (i == 0 && j == 0)
+						continue;
+					else if (i == n - 1 && j == 0)
+						continue;
+					else if (i == n - 1 && j == n - 1)
+						continue;
 					sx = i;
 					sy = j;
 					check[a[i][j]] = true;
@@ -52,7 +58,7 @@ public class Solution {
 					check[a[i][j]] = false;
 				}
 			}
-			System.out.printf("#%d %d\n",test_case,ans);
+			System.out.printf("#%d %d\n", test_case, ans);
 		}
 	}
 }
