6c8d04140251a6bb944a69798b04516c74a20854
tongnamuu <33937684+thban33@users.noreply.github.com>
2019-03-28 10:37:21 +0900

    SWEA 1952. [모의 SW 역량테스트] 수영장

---
+++ S1952/S1952.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int min(const int& a, const int& b) { return a > b ? b : a; }
+int main() {
+	ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+	int T; cin >> T;
+	for (int test_case = 1; test_case <= T; ++test_case) {
+		int a[4], b[13] = { 0, };
+		int d[13] = { 0, };
+		for (int i = 0; i < 4; ++i) cin >> a[i];//1일  1달 3달 1년
+		for (int i = 1; i <= 12; ++i) cin >> b[i];
+		for (int i = 1; i <= 12; ++i) {
+			if (i >= 3) {
+				if (b[i] * a[0] >= a[1])d[i] = min(d[i - 1] + a[1], d[i - 3] + a[2]);
+				else d[i] = min(d[i - 1] + b[i] * a[0], d[i - 3] + a[2]);
+			}
+			else d[i] = min(d[i - 1] + a[1], d[i - 1] + b[i] * a[0]);
+		}
+		int ans = min(a[3], d[12]);
+		cout << '#' << test_case << ' ' << ans << '\n';
+	}
+}
