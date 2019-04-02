f27bd91256eb9f333294a6b3d616aa1ead6567b2
tongnamuu <33937684+thban33@users.noreply.github.com>
2019-04-02 12:55:06 +0900

    SWEA 4261. 빠른 휴대전화 키패드

--- S4261/S4261.cpp
+++ S4261/S4261.cpp
@@ -2,11 +2,6 @@
 #include <string>
 using namespace std;
 const char alp[] = { '2','2','2','3','3','3','4','4','4','5','5','5','6','6','6','7','7','7','7','8','8','8','9','9','9','9' };
-string tonum(const string& a) {
-	string temp;
-	for(int i=0;a[i];++i) temp+=alp[a[i] - 'a'];
-	return temp;
-}
 int main() {
 	ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
 	int T; cin >> T;
@@ -15,9 +10,15 @@ int main() {
 		int n; cin >> n;
 		int ans = 0;
 		for (int i = 0; i < n; ++i) {
-			string p;cin >> p;
-			string temp = tonum(p);
-			if (temp == s) ++ans;
+			string p; cin >> p;
+			bool go = true;
+			for (int j = 0; p[j]; ++j) {
+				if (alp[p[j] - 'a'] != s[j]) {
+					go = false;
+					break;
+				}
+			}
+			ans += go;
 		}
 		cout << '#' << test_case << ' ' << ans << '\n';
 	}
