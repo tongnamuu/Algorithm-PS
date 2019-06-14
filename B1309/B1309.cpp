32f9b38b7e129496a99cfa3c719fe14ebb7fb953
tongnamuu <33937684+thban33@users.noreply.github.com>
2019-06-14 16:44:16 +0900

    BOJ 1309 동물원

---
+++ B1309/B1309.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int d[100001][3];
+int main() {
+	int n; cin >> n;
+	d[0][0] = 1;
+	d[1][0] = d[1][1] = d[1][2] = 1;
+	for (int i = 2; i <= n; ++i) {
+		d[i][0] = (d[i - 1][0] + d[i - 1][1] + d[i - 1][2]) % 9901;
+		d[i][1] = (d[i - 1][0] + d[i - 1][2]) % 9901;
+		d[i][2] = (d[i - 1][0] + d[i - 1][1]) % 9901;
+	}
+	cout << (d[n][0] + d[n][1] + d[n][2]) % 9901 << '\n';
+}
