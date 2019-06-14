66b6ac8234706b3bde5931c5db01a46ac77293f6
tongnamuu <33937684+thban33@users.noreply.github.com>
2019-06-15 03:27:22 +0900

    BOJ 5101 Sequences

--- B5101/B5101.cpp
+++ B5101/B5101.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-#include <string>
 using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
-	string s; cin >> s;
-	for (int i = 0; s[i]; ++i) {
-		cout << (char)('A' + ((s[i] - 'A' + 23) % 26));
+	int a, b, c;
+	while (true) {
+		cin >> a >> b >> c;
+		if (a == 0 && b == 0 && c == 0) break;
+		if ((c - a) % b == 0) cout << (c - a) / b + 1 << '\n';
+		else cout << 'X' << '\n';
 	}
 }
