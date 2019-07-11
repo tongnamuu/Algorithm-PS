05edf45bd18bcbffb93dc69eb14a479ae3ff778e
tongnamuu <33937684+thban33@users.noreply.github.com>
2019-07-11 13:54:22 +0900

    BOJ 17298 오큰수

--- B17298/B17298.cpp
+++ B17298/B17298.cpp
@@ -7,8 +7,8 @@ int main() {
 	ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
 	memset(d, -1, sizeof(d));
 	int n, num; cin >> n;
-	stack<pair<int, int>>s; cin >> num; s.push({ num,0 });
-	for (int i = 1; i < n; ++i) {
+	stack<pair<int, int>>s;
+	for (int i = 0; i < n; ++i) {
 		cin >> num;
 		while (!s.empty() && num > s.top().first) {
 			d[s.top().second] = num;
