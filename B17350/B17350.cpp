c2a6d59427ac9cc933dfa6b19393f926a8d219d8
tongnamuu <33937684+thban33@users.noreply.github.com>
2019-07-30 14:58:27 +0900

    BOJ 17350 2루수 이름이 뭐야

---
+++ B17350/B17350.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+	int n; cin >> n;
+	string name;
+	for (int i = 0; i < n; ++i) {
+		cin >> name;
+		if (name == "anj") {
+			cout << "构具;" << '\n';
+			return 0;
+		}
+	}
+	cout << "构具?" << '\n';
+}
