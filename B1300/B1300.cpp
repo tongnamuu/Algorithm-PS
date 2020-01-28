027a926a5af9c0214b73e3fd745efd7b087d960a
tongnamuu <thban33@naver.com>
2020-01-29 01:50:40 +0900

    BOJ 1300 K번째 수

---
+++ B1300/B1300.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+	int n, k; cin >> n >> k;
+	int start = 1;
+	long long end = 1000000000;
+	while (start <= end) {
+		int mid = start + end >> 1;
+		int cnt = 0;
+		int same = 0;
+		for (int i = 1; i <= n; ++i) {
+			cnt += mid / i > n ? n : mid / i;
+			if (mid%i == 0 && mid / i <= n) ++same;
+		}
+		if (k <= cnt) {
+			end = mid - 1;
+			if (cnt - same < k) {
+				cout << mid << '\n';
+				return 0;
+			}
+		}
+		else {
+			start = mid + 1;
+		}
+	}
+	cout << start << '\n';
+}
