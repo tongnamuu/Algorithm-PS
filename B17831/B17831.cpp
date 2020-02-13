40d75ae91c10ade114b215d757327760523e8bbf
tongnamuu <thban33@naver.com>
2020-02-13 18:54:31 +0900

    BOJ 17831 대기업 승범이네

--- B17831/B17831.cpp
+++ B17831/B17831.cpp
@@ -8,18 +8,17 @@ int p[SIZE];
 vector<int>adj[SIZE];
 int a[SIZE];
 int d[SIZE][2];
-int go(int now, int parent, int turn) {
+int go(int now,int turn) {
 	if (d[now][turn] != -1) return d[now][turn];
 	int& ret = d[now][turn];
 	ret = 0;
 	for (int next : adj[now]) {
-		if (next == parent) continue;
-		ret += go(next, now, 0);
+		ret += go(next, 0);
 	}
 	if (!turn) {
 		int temp = 0;
 		for (int next : adj[now]) {
-			temp = max(ret - go(next, now, 0) + go(next, now, 1) + a[now] * a[next], temp);
+			temp = max(ret - go(next, 0) + go(next, 1) + a[now] * a[next], temp);
 		}
 		ret = max(ret, temp);
 	}
@@ -36,5 +35,5 @@ int main() {
 		cin >> a[i];
 	}
 	memset(d, -1, sizeof(d));
-	cout << go(1, 0, 0);
+	cout << go(1, 0);
 }
