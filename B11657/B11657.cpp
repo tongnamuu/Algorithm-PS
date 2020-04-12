edf7d7453397a3a8202be1ea5a211091bb8371d7
tongnamuu <thban33@naver.com>
2020-04-12 17:51:14 +0900

    BOJ 11657 타임머신

--- B11657/B11657.cpp
+++ B11657/B11657.cpp
@@ -1,38 +1,38 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-struct edge {
-	int from, to, cost;
+#define INF 1000000000
+struct Edge {
+	int to, cost;
 };
-edge a[6000];
+vector<Edge>adj[501];
 int d[501];
-const int inf = 1000000000;
 int main() {
 	ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
 	int n, m; cin >> n >> m;
-	for (int i = 2; i <= n; ++i) d[i] = inf;
-	for (int i = 0; i < m; ++i) {
-		cin >> a[i].from >> a[i].to >> a[i].cost;
+	for (int i = 2; i <= n; ++i) d[i] = INF;
+	for (int i = 0, u, v, c; i < m; ++i) {
+		cin >> u >> v >> c;
+		adj[u].push_back({ v,c });
 	}
 	bool cycle = false;
 	for (int i = 1; i <= n; ++i) {
-		for (int j = 0; j < m; ++j) {
-			int x = a[j].from;
-			int y = a[j].to;
-			int z = a[j].cost;
-			if (d[x] != inf && d[y] > d[x] + z) {
-				d[y] = d[x] + z;
-				if (i == n) {
-					cycle = true;
+		for (int now = 1; now <= n; ++now) {
+			for (auto p : adj[now]) {
+				int next = p.to;
+				int cost = p.cost;
+				if (d[now] != INF && d[next] > d[now] + cost) {
+					d[next] = d[now] + cost;
+					if (i == n) cycle = true;
 				}
 			}
 		}
 	}
-	if (cycle) cout << -1 << '\n';
+	if (cycle) cout << -1;
 	else {
 		for (int i = 2; i <= n; ++i) {
-			if (d[i] != inf) cout << d[i] << '\n';
-			else cout << -1 << '\n';
+			if (d[i] == INF) d[i] = -1;
+			cout << d[i] << '\n';
 		}
 	}
 }
