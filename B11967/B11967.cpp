0610d3458b8f1a89252dea2cae3ab6db3ada57de
tongnamuu <thban33@naver.com>
2020-05-28 00:30:18 +0900

    BOJ 11967 불켜기

--- B11967/B11967.cpp
+++ B11967/B11967.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <algorithm>
 using namespace std;
-int d[101][101];
-bool bright[101][101];
-bool InQ[101][101];
-vector<int>adj[11001];
+#define N 101
+int d[N][N];
+bool bright[N][N];
+bool InQ[N][N];
+vector<pair<int, int>>adj[N][N];
 const int dx[] = { -1,1,0,0 };
 const int dy[] = { 0,0,-1,1 };
 int main() {
@@ -14,20 +14,20 @@ int main() {
     int n, m; cin >> n >> m;
     for (int i = 0, x1, y1, x2, y2; i < m; ++i) {
         cin >> x1 >> y1 >> x2 >> y2;
-        adj[x1 * 101 + y1].push_back(x2 * 101 + y2);
+        adj[x1][y1].push_back({ x2,y2 });
     }
     queue<pair<int, int>>q;
     d[1][1] = 1;
     q.push({ 1,1 });
-    int ans = 0;
+    bright[1][1] = true;
+    InQ[1][1] = true;
     while (!q.empty()) {
         int x = q.front().first;
         int y = q.front().second;
         q.pop();
-        ++ans;
-        for (int next : adj[x * 101 + y]) {
-            int t1 = next / 101;
-            int t2 = next % 101;
+        for (auto next : adj[x][y]) {
+            int t1 = next.first;
+            int t2 = next.second;
             bright[t1][t2] = true;
             if (InQ[t1][t2] && !d[t1][t2]) {
                 d[t1][t2] = 1;
@@ -37,7 +37,7 @@ int main() {
         for (int dir = 0; dir < 4; ++dir) {
             int nx = x + dx[dir];
             int ny = y + dy[dir];
-            if (nx > 0 && nx <= n && ny > 0 && ny <= n) {
+            if (nx >= 1 && nx <= n && ny >= 1 && ny <= n) {
                 InQ[nx][ny] = true;
                 if (bright[nx][ny] && !d[nx][ny]) {
                     d[nx][ny] = 1;
@@ -46,5 +46,11 @@ int main() {
             }
         }
     }
-    cout << min(ans,n*n);
+    int ans = 0;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            ans += bright[i][j];
+        }
+    }
+    cout << ans << '\n';
 }
