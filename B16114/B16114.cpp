1968f34b4bcf5e8d94306cd8e21603b92e2db8df
tongnamuu <thban33@naver.com>
2020-06-23 00:04:19 +0900

    BOJ 16114 화살표 연산자

--- B16114/B16114.cpp
+++ B16114/B16114.cpp
@@ -7,7 +7,7 @@ int main() {
         if (n == 1) cout << "INFINITE";
         else if (n == 0) cout << 0;
         else if (n & 1) cout << "ERROR";
-        else cout << "ERROR";
+        else cout << 0;
     }
     else if (x == 0) {
         if (n == 1) cout << 0;
@@ -21,13 +21,7 @@ int main() {
         else if (n & 1) cout << "ERROR";
         else {
             n >>= 1;
-            int cnt = 0;
-            x -= n;
-            while (x > 0) {
-                x -= n;
-                ++cnt;
-            }
-            cout << cnt;
+            cout << (x - 1) / n;
         }
     }
 }
