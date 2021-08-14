31989d8e1f1fba7b3d6fc15be5a16dc027928e37
tongnamuu <thban33@naver.com>
2021-08-14 23:58:39 +0900

    BOJ 17609 회문

---
+++ B17609/B17609.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+bool isP(const string& s, int i, int j) {
+    while (i < j) {
+        if (s[i] != s[j]) return false;
+        i++; j--;
+    }
+    return true;
+}
+bool isP1(const string& s) {
+    int i = 0, j = s.length() - 1;
+    int change = 0;
+    while (i < j) {
+        if (s[i] == s[j]){
+            i++;
+            j--;
+        }
+        else {
+            if (isP(s, i, j - 1)) return true;
+            else if (isP(s, i + 1, j)) return true;
+            return false;
+        }
+    }
+    return false;
+}
+int solve(const string& s) {
+    if (isP(s, 0, s.length() - 1)) return 0;
+    if (isP1(s)) return 1;
+    return 2;
+}
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(0);
+    int n; cin >> n;
+    string s;
+    for (int i = 0; i < n; ++i) {
+        cin >> s;
+        cout << solve(s) << '\n';
+    }
+}
